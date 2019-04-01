0b35be13066bc59e17fea6a1593eea59dc84b88c
Greg Foreman <gfore@mindspring.com>
2019-03-31 23:33:10 -0400

    converts Square to function component and adds O

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -20,26 +15,30 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
-    handleCLick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+    handleClick(i) {
+        const squares = this.state.squares.slice();  // returns a shallow copy of this.state.squares array into new variable squares
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
         return (
             <Square
                 value={this.state.squares[i]}
-                onClick={() => this.handleCLick(i)}              
-            />)
-        ;
+                onClick={() => this.handleClick(i)}              
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
             <div>
