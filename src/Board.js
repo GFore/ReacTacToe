b387a984f1d90742e4c21357d920afb6394cd5b9
Greg Foreman <gfore@mindspring.com>
2019-04-20 00:36:47 -0400

    refactor Board to use nested loops to create the board

--- src/Board.js
+++ src/Board.js
@@ -2,43 +2,41 @@ import React from 'react';
 
 function Square(props) {
   return (
-      <button className={`square ${props.hilite ? 'highlighted' : ''}`} onClick={props.onClick}>
-          {props.value}
-      </button>
+    <button className={`square ${props.hilite ? 'highlighted' : ''}`} onClick={props.onClick}>
+      {props.value}
+    </button>
   );
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-      return (
-          <Square
-              hilite={this.props.highlighted[i]}
-              value={this.props.squares[i]}
-              onClick={() => this.props.onClick(i)}              
-          />
-      );
+    return (
+      <Square
+        hilite={this.props.highlighted[i]}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}              
+      />
+    );
+  }
+
+  renderBoard = () => {
+    let board = [];
+
+    for (let i = 0; i < 3; i++) {
+      let rows = [];
+      for (let j = (i * 3); j < (i+1)*3; j++) {
+        rows.push(this.renderSquare(j))
+      }
+      
+      board.push(<div className="board-row">{rows}</div>)
+    }
+    return board;
   }
 
   render() {
-      return (
-          <div>
-              <div className="board-row">
-                  {this.renderSquare(0)}
-                  {this.renderSquare(1)}
-                  {this.renderSquare(2)}
-              </div>
-              <div className="board-row">
-                  {this.renderSquare(3)}
-                  {this.renderSquare(4)}
-                  {this.renderSquare(5)}
-              </div>
-              <div className="board-row">
-                  {this.renderSquare(6)}
-                  {this.renderSquare(7)}
-                  {this.renderSquare(8)}
-              </div>
-          </div>
-      );
+    return (
+      <div>{this.renderBoard()}</div>
+    );
   }
 }
 
