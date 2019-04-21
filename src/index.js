43561b44af1b4ef39517c41c532860298d012192
Greg Foreman <gfore@mindspring.com>
2019-04-21 19:14:33 -0400

    handles a tie game

--- src/index.js
+++ src/index.js
@@ -115,6 +115,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = `Winner: ${winner.player}`;
+        } else if (!current.squares.includes(null)) {
+            status = `Tie game! Cat Wins!`;
         } else {
             status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
         }
