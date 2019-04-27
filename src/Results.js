cb6a8776fb2cc30de478ee3360ce8e4f37248d0d
Greg Foreman <gfore@mindspring.com>
2019-04-27 17:46:57 -0400

    responsive styling

--- src/Results.js
+++ src/Results.js
@@ -1,39 +1,7 @@
 import React from 'react';
 
-// function Square(props) {
-//   return (
-//     <button className={`square ${props.hilite ? 'highlighted' : ''}`} onClick={props.onClick}>
-//       {props.value}
-//     </button>
-//   );
-// }
 
 class Results extends React.Component {
-  // renderSquare(i) {
-  //   return (
-  //     <Square
-  //       hilite={this.props.highlighted[i]}
-  //       value={this.props.squares[i]}
-  //       onClick={() => this.props.onClick(i)}
-  //       key={i}         
-  //     />
-  //   );
-  // }
-
-  // renderBoard = () => {
-  //   let board = [];
-
-  //   for (let i = 0; i < 3; i++) {
-  //     let rows = [];
-  //     for (let j = (i * 3); j < (i+1)*3; j++) {
-  //       rows.push(this.renderSquare(j))
-  //     }
-      
-  //     board.push(<div className="board-row" key={i}>{rows}</div>)
-  //   }
-  //   return board;
-  // }
-
   render() {
     const {results} = this.props; // p1Wins, p2Wins, ties}
     const played = results.p1Wins + results.p2Wins + results.ties;
