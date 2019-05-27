ed1654ba25a4c9852a1c9587571d631959b83dde
Greg Foreman <gfore@mindspring.com>
2019-05-27 19:59:06 -0400

    Updates

---
+++ precache-manifest.2a1fa2cd31e00c0cb12d8d2cf2a3f3cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd00a3095990754bdd9",
+    "url": "/ReacTacToe/static/js/runtime~main.845c4d7c.js"
+  },
+  {
+    "revision": "96cf1896510a644dd935",
+    "url": "/ReacTacToe/static/js/main.0ed8c424.chunk.js"
+  },
+  {
+    "revision": "fd07a9908647dbe7ff57",
+    "url": "/ReacTacToe/static/js/2.fa0cffb4.chunk.js"
+  },
+  {
+    "revision": "96cf1896510a644dd935",
+    "url": "/ReacTacToe/static/css/main.e60b1035.chunk.css"
+  },
+  {
+    "revision": "afb624225d0c4a85c1fd26740242f577",
+    "url": "/ReacTacToe/index.html"
+  }
+];
