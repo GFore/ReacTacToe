0a028c79fb25aba9b85e71a7f037aab496fdd5a7
Greg Foreman <gfore@mindspring.com>
2022-05-29 12:30:12 -0400

    split off canUseLocalStorage to utils

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -8,35 +8,8 @@ import GameInfo from './GameInfo.js'
 import MoveButton from './MoveButton.js'
 import Results from './Results.js'
 import { colorP1, colorP2, colorTie } from './constants';
+import { canUseLocalStorage } from './utils';
 
-const storageAvailable = () => {
-  // Modified from MDN code found at:
-  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Testing_for_availability
-  let storage;
-  try {
-    storage = window['localStorage'];
-    const x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  }
-  catch(e) {
-    return e instanceof DOMException && (
-      // everything except Firefox
-      e.code === 22 ||
-      // Firefox
-      e.code === 1014 ||
-      // test name field too, because code might not be present
-      // everything except Firefox
-      e.name === 'QuotaExceededError' ||
-      // Firefox
-      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-      // acknowledge QuotaExceededError only if there's something already stored
-      (storage && storage.length !== 0);
-  }
-}
-
-const canUseLocalStorage = storageAvailable();
 const initialState = {
   highlighted: Array(9).fill(false),
   history: [{squares: Array(9).fill(null)}],
---
+++ src/Components/utils.js
@@ -0,0 +1,28 @@
+const storageAvailable = () => {
+  // Modified from MDN code found at:
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Testing_for_availability
+  let storage;
+  try {
+    storage = window['localStorage'];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  }
+  catch(e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      (storage && storage.length !== 0);
+  }
+}
+
+export const canUseLocalStorage = storageAvailable();
