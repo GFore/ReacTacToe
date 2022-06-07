4f4e6b0fd9043248690046c8788429b9ffb1b312
Greg Foreman <gfore@mindspring.com>
2022-06-07 13:45:05 -0400

    cleanup NivoPieChart

--- src/Components/NivoPieChart.js
+++ src/Components/NivoPieChart.js
@@ -4,31 +4,26 @@ import { colorTextSecondary } from './constants';
 
 const NivoPieChart = ({ data, colors }) => (
   <ResponsivePie
-      data={data}
-      colors={colors}
-      margin={{ 'top': 20, 'right': 0, 'bottom': 40, 'left': -20 }}
-      borderColor={{ 'from': 'color', 'modifiers': [['darker', 0.2]] }}
-      enableRadialLabels={false}
-      slicesLabelsSkipAngle={10}
-      slicesLabelsTextColor={colorTextSecondary}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
+    data={data}
+    colors={colors}
+    margin={{ 'top': 20, 'right': 0, 'bottom': 40, 'left': -20 }}
+    borderColor={{ 'from': 'color', 'modifiers': [['darker', 0.2]] }}
+    enableRadialLabels={false}
+    slicesLabelsSkipAngle={10}
+    slicesLabelsTextColor={colorTextSecondary}
+    animate={true}
+    motionStiffness={90}
+    motionDamping={15}
 
-      tooltip={({ id, value, color }) => (
-        <strong style={{ color }}>
-          {id}: {value}
-        </strong>
-      )}
-      theme={{
-        labels: { text: { fontSize: 25, }},
-        tooltip: {
-          container: {
-            background: colorTextSecondary,
-            borderRadius: '10px',
-          },
-        },
-      }}
+    tooltip={({ id, value, color }) => (
+      <strong style={{ color }}>{id}: {value}</strong>
+    )}
+    theme={{
+      labels: { text: { fontSize: 25 }},
+      tooltip: {
+        container: { background: colorTextSecondary, borderRadius: '10px' },
+      },
+    }}
   />
 );
 
