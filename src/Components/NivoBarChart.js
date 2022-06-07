f3abacffc2d5c67bd9f216ca8eb1acb96a2573d3
Greg Foreman <gfore@mindspring.com>
2022-06-07 13:40:54 -0400

    cleanup NivoLineChart

--- src/Components/NivoBarChart.js
+++ src/Components/NivoBarChart.js
@@ -8,7 +8,7 @@ const theme = {
     fontSize: "14px",
     tickColor: "#eee",
     ticks: {
-      line: { stroke: "#555555" },
+      line: { stroke: "#555" },
       text: { fill: "#fff" },
     },
     legend: {
@@ -28,27 +28,27 @@ const theme = {
 
 const NivoBarChart = ({ data, maxValue }) => (
   <ResponsiveBar
-      data={data}
-      indexBy='player'
-      keys={['p1', 'p2', 'ties']}
-      maxValue={maxValue}
-      colorBy="id"
-      colors={({ id, data }) => data[`${id}Color`]}
-      margin={{ 'top': 30, 'right': 10, 'bottom': 70, 'left': 20 }}
-      padding={0.35}
-      borderColor={{ 'from': 'color', 'modifiers': [['darker', 0.2]] }}
-      enableLabel={false}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{ tickSize: 5, tickPadding: 5, tickRotation: 0 }}
-      axisLeft={{ tickSize: 1, tickPadding: 5, tickRotation: 0 }}
-      tooltip={({ id, value, color }) => (
-        <strong style={{ color }}>{id}{id === 'ties' ? '' : ' wins'}: {value}</strong>
-      )}
-      theme={theme}
+    data={data}
+    indexBy='player'
+    keys={['p1', 'p2', 'ties']}
+    maxValue={maxValue}
+    colorBy="id"
+    colors={({ id, data }) => data[`${id}Color`]}
+    margin={{ 'top': 30, 'right': 10, 'bottom': 70, 'left': 20 }}
+    padding={0.35}
+    borderColor={{ 'from': 'color', 'modifiers': [['darker', 0.2]] }}
+    enableLabel={false}
+    animate={true}
+    motionStiffness={90}
+    motionDamping={15}
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{ tickSize: 5, tickPadding: 5, tickRotation: 0 }}
+    axisLeft={{ tickSize: 1, tickPadding: 5, tickRotation: 0 }}
+    tooltip={({ id, value, color }) => (
+      <strong style={{ color }}>{id}{id === 'ties' ? '' : ' wins'}: {value}</strong>
+    )}
+    theme={theme}
   />
 );
 
--- src/Components/NivoLineChart.js
+++ src/Components/NivoLineChart.js
@@ -8,24 +8,12 @@ const theme = {
     fontSize: "14px",
     tickColor: "#eee",
     ticks: {
-      line: {
-        stroke: "#555555"
-      },
-      text: {
-        fill: "#ffffff"
-      }
+      line: { stroke: "#555" },
+      text: { fill: "#fff" },
     },
-    legend: {
-      text: {
-        fill: "#aaaaaa"
-      }
-    }
+    legend: { text: { fill: "#aaa" } },
   },
-  grid: {
-    line: {
-      stroke: "#777"
-    }
-  },
+  grid: { line: { stroke: "#777" } },
   tooltip: {
     container: {
       background: colorTextSecondary,
@@ -36,53 +24,51 @@ const theme = {
 
 const NivoLineChart = ({ data, colors }) => (
   <ResponsiveLine
-      data={data}
-      curve={"monotoneX"}
-      // gridYValues={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
+    data={data}
+    curve={"monotoneX"}
+    // gridYValues={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
 
-      margin={{ top: 30, right: 10, bottom: 90, left: 45 }}
-      xScale={{ type: 'point' }}
-      yScale={{ type: 'linear', stacked: false, min: 0, max: 'auto' }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-          orient: 'bottom',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Games Played',
-          legendOffset: 36,
-          legendPosition: 'middle'
-      }}
-      axisLeft={{
-          orient: 'left',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          tickValues: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-          legend: 'Count',
-          legendOffset: -40,
-          legendPosition: 'middle'
-      }}
-      colors={colors}
-      pointSize={4}
-      pointColor={{ from: 'color', modifiers: [] }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: 'serieColor', modifiers: [] }}
-      pointLabel="y"
-      pointLabelYOffset={-12}
-      enableSlices="x"
+    margin={{ top: 30, right: 10, bottom: 90, left: 45 }}
+    xScale={{ type: 'point' }}
+    yScale={{ type: 'linear', stacked: false, min: 0, max: 'auto' }}
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      orient: 'bottom',
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'Games Played',
+      legendOffset: 36,
+      legendPosition: 'middle'
+    }}
+    axisLeft={{
+      orient: 'left',
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      tickValues: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      legend: 'Count',
+      legendOffset: -40,
+      legendPosition: 'middle'
+    }}
+    colors={colors}
+    pointSize={4}
+    pointColor={{ from: 'color', modifiers: [] }}
+    pointBorderWidth={2}
+    pointBorderColor={{ from: 'serieColor', modifiers: [] }}
+    pointLabel="y"
+    pointLabelYOffset={-12}
+    enableSlices="x"
 
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
+    animate={true}
+    motionStiffness={90}
+    motionDamping={15}
 
-      tooltip={({ id, value, color }) => (
-        <strong style={{ color }}>
-          {id}{id === 'ties' ? '' : ' wins'}: {value}
-        </strong>
-      )}
-      theme={theme}
+    tooltip={({ id, value, color }) => (
+      <strong style={{ color }}>{id}{id === 'ties' ? '' : ' wins'}: {value}</strong>
+    )}
+    theme={theme}
   />
 );
 
