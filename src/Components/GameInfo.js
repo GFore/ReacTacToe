59d169362155381021fdfe91b0075a7fb9e53519
Greg Foreman <gfore@mindspring.com>
2022-07-02 19:18:59 -0400

    convert to MUI buttons

--- src/Components/GameInfo.js
+++ src/Components/GameInfo.js
@@ -3,42 +3,50 @@ import Power from '@material-ui/icons/PowerSettingsNew';
 import Shuffle from '@material-ui/icons/Shuffle'; 
 import SwapVert from '@material-ui/icons/SwapVert'; 
 import Undo from '@material-ui/icons/Undo'; 
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-const GameInfo = ({ historyLength, moves, playerOneIsX, sortMovesAscending, status, switchPlayers, undoLastMove, updateState }) => (
-  <div className="game-info">
-    <div className="game-status"><h2>{status}</h2></div>
-    <div className="move-list">
-      <h4>Moves</h4>
-      <hr />
-      <ol>{sortMovesAscending ? moves : moves.reverse()}</ol>
+const GameInfo = ({ historyLength, moves, playerOneIsX, sortMovesAscending, status, switchPlayers, undoLastMove, updateState }) => {
+  const hasMoves = historyLength !== 1;
+  const cannotUndo = !hasMoves || !status.startsWith('Next');
+  
+  return (
+    <div className="game-info">
+      <div className="game-status"><h2>{status}</h2></div>
+      <div className="move-list">
+        <h4>Moves</h4>
+        <hr />
+        <ol>{sortMovesAscending ? moves : moves.reverse()}</ol>
+      </div>
+      <ButtonGroup variant="contained" className='game-btn-group'>
+        <Button title="Start New Game" onClick={() => updateState({playerOneIsX: playerOneIsX}, true)}><Power /></Button>
+        <Button
+          title="Switch Players"
+          onClick={switchPlayers}
+          disabled={hasMoves}
+          style={hasMoves ? { backgroundColor: '#e0e0e0'} : null }
+        >
+          <Shuffle />
+        </Button>
+        <Button
+          title="Undo Last Move"
+          onClick={undoLastMove}
+          disabled={cannotUndo}
+          style={cannotUndo ? { backgroundColor: '#e0e0e0'} : null }
+        >
+          <Undo />
+        </Button>
+        <Button
+          title={sortMovesAscending ? "Sort Moves Descending" : "Sort Moves Ascending"}
+          onClick={() => updateState({sortMovesAscending: !sortMovesAscending})}
+          disabled={!hasMoves}
+          style={hasMoves ? null : { backgroundColor: '#e0e0e0'}}
+        >
+          <SwapVert />
+        </Button>
+      </ButtonGroup>
     </div>
-    <div className="game-board-buttons">
-      <button title="Start New Game"
-        onClick={() => updateState({playerOneIsX: playerOneIsX}, true)}
-      >
-        <Power />
-      </button>
-      <button title="Switch Players"
-        onClick={() => switchPlayers()}
-        disabled={historyLength !== 1}
-      >
-        <Shuffle />
-      </button>
-      <button title="Undo Last Move"
-        onClick={() => undoLastMove()}
-        disabled={historyLength === 1 || !status.startsWith('Next')}
-      >
-        <Undo />
-      </button>
-      <button
-        title={sortMovesAscending ? "Sort Moves Descending" : "Sort Moves Ascending"}
-        onClick={() => updateState({sortMovesAscending: !sortMovesAscending})}
-        disabled={historyLength === 1}
-      >
-        <SwapVert />
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default GameInfo;
