cb21c92bfc8996032f44b46ec35c7ac5a6207a7a
Greg Foreman <gfore@mindspring.com>
2022-07-03 22:11:13 -0400

    cleanup

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -4,33 +4,3 @@ export const colorP2 = 'cornflowerBlue';
 export const colorTie = 'lightGoldenRodYellow';
 export const colorTextPrimary = 'whitesmoke';
 export const colorTextSecondary = '#333333';
-
-
-// SAMPLE PIE CHART DATA
-// const data = [
-//   {
-//     "id": "stylus",
-//     "label": "stylus",
-//     "value": 289,
-//   },
-//   {
-//     "id": "scala",
-//     "label": "scala",
-//     "value": 184,
-//   },
-//   {
-//     "id": "php",
-//     "label": "php",
-//     "value": 12,
-//   },
-//   {
-//     "id": "hack",
-//     "label": "hack",
-//     "value": 428,
-//   },
-//   {
-//     "id": "java",
-//     "label": "java",
-//     "value": 323,
-//   }
-// ]
