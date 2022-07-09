36f56653e7f892df25a431953fbe2e2007083fe0
Greg Foreman <gfore@mindspring.com>
2022-07-09 00:52:44 -0400

    fix breaking version changes for nivo pie chart

--- src/Components/NivoPieChart.js
+++ src/Components/NivoPieChart.js
@@ -8,23 +8,20 @@ const NivoPieChart = ({ data, colors }) => (
       data={data}
       colors={colors}
       margin={{ 'top': 20, 'right': 0, 'bottom': 40, 'left': -20 }}
+      cornerRadius={2}
       borderColor={{ 'from': 'color', 'modifiers': [['darker', 0.2]] }}
-      enableRadialLabels={false}
-      slicesLabelsSkipAngle={10}
-      slicesLabelsTextColor={colorTextSecondary}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
-  
-      tooltip={({ id, value, color }) => (
-        <strong style={{ color }}>{id}: {value}</strong>
-      )}
-      theme={{
-        labels: { text: { fontSize: 25 }},
-        tooltip: {
-          container: { background: colorTextSecondary, borderRadius: '10px' },
-        },
-      }}
+      enableArcLinkLabels={false}
+      arcLabel="value"
+      arcLabelsRadiusOffset={0.6}
+      arcLabelsSkipAngle={40}
+      arcLabelsTextColor={colorTextSecondary}
+      animate={false}
+      theme={{ labels: { text: { fontSize: 25 }} }}
+      tooltip={({datum: { id, value, color }}) => (
+        <div style={{ padding: 6, backgroundColor: colorTextSecondary, borderRadius: 10 }}>
+          <strong style={{ color }}>{id}: {value}</strong>
+        </div>
+      )}      
     />
   </div>
 );
