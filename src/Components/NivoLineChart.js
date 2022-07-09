5252548f14d82d356c11eeaf62e4e3a906a678f0
Greg Foreman <gfore@mindspring.com>
2022-07-09 18:47:12 -0400

    fix breaking version changes for nivo line chart

--- src/Components/NivoLineChart.js
+++ src/Components/NivoLineChart.js
@@ -3,7 +3,6 @@ import { ResponsiveLine } from '@nivo/line';
 import { colorTextSecondary } from './constants';
 
 const theme = {
-  // background: "#222222",
   axis: {
     fontSize: "14px",
     tickColor: "#eee",
@@ -22,56 +21,61 @@ const theme = {
   },
 };
 
-const NivoLineChart = ({ data, colors }) => (
-  <div className="chartWrapper">
-    <ResponsiveLine
-      data={data}
-      curve={"monotoneX"}
-      // gridYValues={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
-  
-      margin={{ top: 30, right: 10, bottom: 90, left: 45 }}
-      xScale={{ type: 'point' }}
-      yScale={{ type: 'linear', stacked: false, min: 0, max: 'auto' }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        orient: 'bottom',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'Games Played',
-        legendOffset: 36,
-        legendPosition: 'middle'
-      }}
-      axisLeft={{
-        orient: 'left',
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        tickValues: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-        legend: 'Count',
-        legendOffset: -40,
-        legendPosition: 'middle'
-      }}
-      colors={colors}
-      pointSize={4}
-      pointColor={{ from: 'color', modifiers: [] }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: 'serieColor', modifiers: [] }}
-      pointLabel="y"
-      pointLabelYOffset={-12}
-      enableSlices="x"
-  
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
-  
-      tooltip={({ id, value, color }) => (
-        <strong style={{ color }}>{id}{id === 'ties' ? '' : ' wins'}: {value}</strong>
-      )}
-      theme={theme}
-    />
-  </div>
-);
+const NivoLineChart = ({ data, colors }) => {
+  const gameCount = data[0].data.length;
+  return (
+    <div className="chartWrapper">
+      <ResponsiveLine
+        data={data}
+        curve={"monotoneX"}   
+        margin={{ top: 30, right: 10, bottom: 90, left: 45 }}
+        xScale={{ type: 'point' }}
+        yScale={{ type: 'linear', stacked: false, min: 0, max: 'auto' }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          tickValues: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+          legend: 'Games Played',
+          legendOffset: 36,
+          legendPosition: 'middle'
+        }}
+        axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          // tickValues: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+          legend: 'Count',
+          legendOffset: -40,
+          legendPosition: 'middle'
+        }}
+        colors={colors}
+        enableGridX={gameCount < 30}
+        enablePoints={gameCount < 30}
+        pointSize={gameCount > 10 ? 2 : 4}
+        pointColor={{ from: 'color', modifiers: [] }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor', modifiers: [] }}
+        enableSlices="x"
+        theme={theme}
+        sliceTooltip={({slice}) => (
+          <div style={{ padding: 8, backgroundColor: colorTextSecondary, borderRadius: 10, fontSize: 12 }}>              
+            {slice.points.map(point => (
+              <div key={point.id} style={{ color: point.serieColor, padding: '2px 0 2px 2px' }}>
+                <strong>{point.data.yFormatted}</strong> {point.serieId === 'ties' ? 'Ties' : `${point.serieId.toUpperCase()} Wins`}
+              </div>
+            ))}
+            <hr />
+            <div>{slice.points[0].data.x} Games Played</div>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
 
 export default NivoLineChart;
