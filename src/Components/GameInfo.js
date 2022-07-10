4c558977f83c601eaf698e6a2abbb59fcf0d3b8c
Greg Foreman <gfore@mindspring.com>
2022-07-10 18:41:25 -0400

    add sort btn to move list header

--- src/Components/GameInfo.js
+++ src/Components/GameInfo.js
@@ -5,10 +5,13 @@ import SwapVert from '@material-ui/icons/SwapVert';
 import Undo from '@material-ui/icons/Undo'; 
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 
 const GameInfo = ({ historyLength, moves, playerOneIsX, sortMovesAscending, status, switchPlayers, undoLastMove, updateState }) => {
   const hasMoves = historyLength !== 1;
   const cannotUndo = !hasMoves || !status.startsWith('Next');
+  const canSort = moves.length > 1;
 
   return (
     <div className="game-info">
@@ -41,7 +44,23 @@ const GameInfo = ({ historyLength, moves, playerOneIsX, sortMovesAscending, stat
       </ButtonGroup>
       <div className="game-status">
         <h2>{status}</h2>
-        <h4>Moves</h4>
+        <Grid container>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={6}><h4>Moves</h4></Grid>
+          <Grid item xs={3} container justifyContent='flex-end'>
+            {canSort &&
+              <IconButton
+                color="inherit"
+                disabled={!hasMoves}
+                onClick={() => updateState({sortMovesAscending: !sortMovesAscending})}
+                style={{ padding: 2 }}
+                title={sortMovesAscending ? "Sort Moves Descending" : "Sort Moves Ascending"}
+              >
+                <SwapVert />
+              </IconButton>
+            }
+          </Grid>
+        </Grid>
       </div>
       <div className="move-list">
         {sortMovesAscending ? moves : moves.reverse()}
