d2a5675b80297ee6149cc0573014fd8b50754f59
Greg Foreman <gfore@mindspring.com>
2022-07-20 17:17:30 -0400

    add useWindowSize custom hook

---
+++ src/Hooks/useWindowSize.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({ height: window.innerHeight, width: window.innerWidth });
+
+  useEffect(() => {
+    const handleResize = () => setWindowSize({ height: window.innerHeight, width: window.innerWidth });
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+export default useWindowSize;
